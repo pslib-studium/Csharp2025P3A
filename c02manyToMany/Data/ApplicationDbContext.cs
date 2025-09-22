3dfdb794b6cd5ff0165c0d4b44423a5dd6209344
Michal Stehlík <michal.stehlik@pslib.cz>
2025-09-22 08:57:20 +0200

    many-to-many

---
+++ c02manyToMany/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using c02manyToMany.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c02manyToMany.Data
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=TheLibrary.db");
+        }
+    }
+}
