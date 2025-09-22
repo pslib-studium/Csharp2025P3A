3c4e373ee62eb74ac801f06f4fc44004bb71cabd
Michal Stehlík <michal.stehlik@pslib.cz>
2025-09-22 15:03:55 +0200

    filter

--- c02manyToMany/Program.cs
+++ c02manyToMany/Program.cs
@@ -1,16 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using c02manyToMany.Data;
+using c02manyToMany.Models;
+using c02manyToMany.Service;
 using Microsoft.EntityFrameworkCore;
 
 ApplicationDbContext context = new ApplicationDbContext();
 context.Database.EnsureDeleted();
 context.Database.EnsureCreated();
 Console.WriteLine("Database created!");
-foreach (var movie in context.Movies.Include(m => m.Artists))
+var repo = new MovieRepository(context);
+
+List<Movie> movies = repo.GetAllMovies(null, 2010);
+
+foreach (var movie in movies)
 {
     Console.WriteLine($"{movie.Title} ({movie.ReleaseYear})");
-    foreach (var artist in movie.Artists)
-    {
-        Console.WriteLine($"\t{artist.FirstName} {artist.LastName}");
-    }
 }
---
+++ c02manyToMany/Service/MovieRepository.cs
@@ -0,0 +1,35 @@
+﻿using c02manyToMany.Data;
+using c02manyToMany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c02manyToMany.Service
+{
+    internal class MovieRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieRepository(ApplicationDbContext? context)
+        {
+            _context = context ??= new ApplicationDbContext();
+        }
+
+        public List<Movie> GetAllMovies(string? title, int? sinceYear)
+        {
+            IQueryable<Movie> query = _context.Movies;
+            // filtry
+            if (!String.IsNullOrEmpty(title))
+            {
+                query = query.Where(m => m.Title.Contains(title));
+            }
+            if (sinceYear.HasValue)
+            {
+                query = query.Where(m => m.ReleaseYear >= sinceYear.Value);
+            }
+            return query.ToList();
+        }
+    }
+}
